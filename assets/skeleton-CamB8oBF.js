4fd770a7cc8787c4026e58e2caee8c58d1cd7fa2
jace200677 <jacefink17@outlook.com>
2026-01-25 15:48:17 -0600

    Delete assets directory

--- assets/skeleton-CamB8oBF.js
+++
@@ -1,9 +0,0 @@
-import {j as m} from "./vendor-ui-ChKirxi5.js";
-import {d as o} from "./index-CGMClAOr.js";
-function n({className: e, ...t}) {
-    return m.jsx("div", {
-        className: o("animate-pulse rounded-md bg-muted", e),
-        ...t
-    })
-}
-export {n as S};
